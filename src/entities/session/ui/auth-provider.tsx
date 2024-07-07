180d2afaaef6708a8de60540bd029757f76b1622
DanilP54 <putrodan@gmail.com>
2024-07-08 02:33:52 +0500

    +

--- src/entities/session/ui/auth-provider.tsx
+++ src/entities/session/ui/auth-provider.tsx
@@ -1,9 +1,10 @@
 import { User } from "firebase/auth";
-import React, { createContext, SetStateAction, useContext, useEffect, useState } from "react";
+import React, { createContext, SetStateAction, useContext, useEffect, useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../services/auth.service";
 import { viewerService } from "@/entities/viewer/interfaces/viewer.services";
 import { Viewer } from "@/entities/viewer/viewer.model";
+
 
 
 type AuthProviderProps = {
@@ -11,7 +12,80 @@ type AuthProviderProps = {
     setIsProcessingAuth: React.Dispatch<SetStateAction<boolean>>;
 }
 
-const AuthContext = createContext<AuthProviderProps | undefined>(undefined)
+enum Step {
+    PHONE_NUMBER_ENTRY = 'PHONE_ENTRY',
+    VERIFY_CODE_ENTRY = 'VERIFY_CODE_ENTRY',
+    USER_DATA_ENTRY = 'USER_DATA_ENTRY',
+}
+
+type StatePhoneNumber = {
+    step: Step.PHONE_NUMBER_ENTRY
+}
+
+type StateVerifyCode = {
+    step: Step.VERIFY_CODE_ENTRY
+}
+
+type StateUserData = {
+    step: Step.USER_DATA_ENTRY,
+    data: string
+}
+
+type State = StatePhoneNumber | StateVerifyCode | StateUserData
+
+const INITIAL_STATE: State = {
+    step: Step.PHONE_NUMBER_ENTRY,
+}
+
+type ActionPhoneNumber = {
+    type: Step.PHONE_NUMBER_ENTRY,
+}
+
+type ActionVerifyCode = {
+    type: Step.VERIFY_CODE_ENTRY
+}
+
+type ActionUserData = {
+    type: Step.USER_DATA_ENTRY,
+    payload: UniqueId
+}
+
+type Actions = ActionPhoneNumber | ActionVerifyCode | ActionUserData
+
+
+const reducer = (state: State, action: Actions): State => {
+    switch (action.type) {
+        case Step.PHONE_NUMBER_ENTRY:
+            return { step: Step.PHONE_NUMBER_ENTRY } as StatePhoneNumber;
+        case Step.VERIFY_CODE_ENTRY:
+            return { step: Step.VERIFY_CODE_ENTRY } as StateVerifyCode;
+        case Step.USER_DATA_ENTRY:
+            return { step: Step.USER_DATA_ENTRY, data: action.payload } as StateUserData;
+        default:
+            throw new Error('Action not found')
+    }
+}
+
+//DispatchContext
+
+const ActionsContext = createContext<React.Dispatch<Actions> | undefined>(undefined)
+
+export const useActionsContext = () => {
+    const context = useContext(ActionsContext)
+
+    if (!context) {
+        throw new Error('Not Found Actions Provider')
+    }
+
+    return context;
+}
+
+
+//StateContext
+
+const AuthContext = createContext<State | undefined>(undefined)
+
+// HOOK
 
 export const useAuthState = () => {
     const context = useContext(AuthContext)
@@ -22,38 +96,41 @@ export const useAuthState = () => {
     return context
 }
 
+// --------------------
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-
-    const [currentViewer, setCurrentViewer] = useState<Viewer | null>(null)
-    const [isProcessingAuth, setIsProcessingAuth] = useState<boolean>(false)
-    const navigate = useNavigate()
+    const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+    // const [currentViewer, setCurrentViewer] = useState<Viewer | null>(null)
+    // const [isProcessingAuth, setIsProcessingAuth] = useState<boolean>(false)
+    // const navigate = useNavigate()
 
     useEffect(() => {
-        const unsubscribe = authService.onAuthState(async (viewer: User) => {
-            try {
-                if (viewer && !isProcessingAuth) {
-                    const docRef = await viewerService.getViewerById(viewer.uid)
-                    if (docRef) {
-                        setCurrentViewer(docRef)
-                        navigate('/home')
-                    }
-                }
-            } catch (error) {
-                console.error(error)
-            }
-        })
-        return () => unsubscribe()
+        // const unsubscribe = authService.onAuthState(async (viewer: User) => {
+        //     try {
+        //         if (viewer && !isProcessingAuth) {
+        //             const docRef = await viewerService.getViewerById(viewer.uid)
+        //             if (docRef) {
+        //                 setCurrentViewer(docRef)
+        //                 navigate('/home')
+        //             }
+        //         }
+        //     } catch (error) {
+        //         console.error(error)
+        //     }
+        // })
+        // return () => unsubscribe()
     }, [])
 
-    const value: AuthProviderProps = {
-        viewer: currentViewer,
-        setIsProcessingAuth,
-    }
+    // const value: AuthProviderProps = {
+    //     viewer: currentViewer,
+    //     setIsProcessingAuth,
+    // }
 
     return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
+        <ActionsContext.Provider value={dispatch}>
+            <AuthContext.Provider value={state}>
+                {children}
+            </AuthContext.Provider>
+        </ActionsContext.Provider>
     )
 }
---
+++ src/features/registration/model/index.tsx
@@ -0,0 +1,41 @@
+import { OTPVerification, PhoneNumber, ViewerInfoForm } from "../ui"
+
+enum Step {
+    PHONE_NUMBER_ENTRY = 'PHONE_ENTRY',
+    VERIFY_CODE_ENTRY = 'VERIFY_CODE_ENTRY',
+    USER_DATA_ENTRY = 'USER_DATA_ENTRY',
+}
+
+type StatePhoneNumber = {
+    step: Step.PHONE_NUMBER_ENTRY
+}
+
+type StateVerifyCode = {
+    step: Step.VERIFY_CODE_ENTRY
+}
+
+type StateUserData = {
+    step: Step.USER_DATA_ENTRY,
+    data: string
+}
+
+type State = StatePhoneNumber | StateVerifyCode | StateUserData
+
+
+
+type StateCreator<T extends State> = (state: T) => React.ReactNode
+
+const createPhoneNumberEntry: StateCreator<StatePhoneNumber> = (state: StatePhoneNumber) => <PhoneNumber />
+const createVerifyCodeEntry: StateCreator<StateVerifyCode> = (state: State) => <OTPVerification {...state} />
+const createUserDataEntry: StateCreator<StateUserData> = (state: State) => <ViewerInfoForm {...state} />
+
+export const routing = (state: State): React.ReactNode => {
+    switch (state.step) {
+        case Step.PHONE_NUMBER_ENTRY:
+            return createPhoneNumberEntry(state);
+        case Step.VERIFY_CODE_ENTRY:
+            return createVerifyCodeEntry(state);
+        case Step.USER_DATA_ENTRY:
+            return createUserDataEntry(state)
+    }
+}
